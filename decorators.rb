a8f9397ac0763d3498662fcaecc8bcde2ef0ae79
dodoburner <100496179+dodoburner@users.noreply.github.com>
2022-09-26 13:02:00 +0200

    Create decorator pattern

---
+++ decorators.rb
@@ -0,0 +1,25 @@
+require './nameable'
+
+class Decorator < Nameable
+  def initialize(nameable)
+    @nameable = nameable
+    super()
+  end
+
+  def correct_name
+    @nameable.correct_name
+  end
+end
+
+class CapitalizeDecorator < Decorator
+  def correct_name
+    @nameable.correct_name.capitalize
+  end
+end
+
+class TrimmerDecorator < Decorator
+  def correct_name
+    res = @nameable.correct_name
+    res.length <= 10 ? res : res[0...10]
+  end
+end
