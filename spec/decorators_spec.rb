471eaed8cdf2913a94fb4c534b5f2bed72e96d8a
dodoburner <100496179+dodoburner@users.noreply.github.com>
2022-10-06 10:54:21 +0200

    Remove comments

--- spec/decorators_spec.rb
+++ spec/decorators_spec.rb
@@ -18,14 +18,3 @@ describe Decorator do
     end
   end
 end
-
-# describe TrimmerDecorator do
-#   person = Person.new(18, 'Desmond@Code360')
-#   trimmer = TrimmerDecorator.new(person)
-
-#   describe '#correct_name' do
-#     it 'returns the first 10 characters of the name' do
-#       expect(trimmer.correct_name).to eq('Desmond@Co')
-#     end
-#   end
-# end
