60cc50efd3e0b51b4dd34efc2a32c285d2bd0ef8
dodoburner <100496179+dodoburner@users.noreply.github.com>
2022-10-06 09:57:59 +0200

    Create test for Person

---
+++ spec/person_spec.rb
@@ -0,0 +1,19 @@
+require_relative '../person'
+
+describe Person do
+  let(:person) { Person.new(18, 'John') }
+
+  describe '#new' do
+    it 'creates a new person' do
+      expect(person).to be_a(Person)
+    end
+
+    it 'has a name' do
+      expect(person.name).to eq('John')
+    end
+
+    it 'has an age' do
+      expect(person.age).to eq(18)
+    end
+  end
+end
